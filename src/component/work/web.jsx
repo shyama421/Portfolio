3e8ab2a00ac4db8bdc115ae23057513e10da949a
Mahek <shyamap227@gmail.com>
2025-01-26 15:06:50 +0530

    Initial commit

--- src/component/work/web.jsx
+++ src/component/work/web.jsx
@@ -1,13 +1,71 @@
 function Web() {
+
+    const projects = [
+        {
+            id: 1,
+            img: <img src="/images/web1.png"><a href="https://construction-website-gamma-ruby.vercel.app/"></a></img>,
+            name: "Furniture Website",
+            lang: "HTML,SCSS,Bootstrap",
+            technologies: {
+                1: "Responsive",
+                2: "Advanced CSS",
+                3: "Animation",
+                4: "Framework"
+            },
+        },
+        {
+            id: 2,
+            img: <img src="/images/web2.png"></img>,
+            name: "Hacker Login Page",
+            lang: "HTML,SCSS",
+            technologies: {
+                1: "Responsive",
+                2: "Advanced CSS",
+                3: "Animation"
+            },
+        },
+        {
+            id: 3,
+            img: <img src="/images/web3.png"></img>,
+            name: "Online Food Order",
+            lang: "HTML,CSS,Advanced CSS",
+            technologies: {
+                1: "Responsive",
+                2: "Advanced CSS",
+                3: "Animation",
+            },
+        },
+        {
+            id: 4,
+            img: <img src="/images/web4.png"></img>,
+            name: "Calculator",
+            lang: "HTML,CSS,JavaScript",
+            technologies: {
+                1: "Functionality",
+                2: "Responsive",
+                3: "Calculation"
+            },
+        }
+
+    ]
+
     return (
         <>
             <div className="grid grid-cols-3 gap-x-5 gap-y-5 pt-6">
-                <img src="/images/1.png" alt="design1" className="h-[300px] animate__animated animate__zoomIn" />
-                <img src="/images/2.png" alt="design2" className="h-[300px] animate__animated animate__zoomIn" />
-                <img src="/images/3.png" alt="design3" className="h-[300px] animate__animated animate__zoomIn" />
-                <img src="/images/4.png" alt="design4" className="h-[300px] animate__animated animate__zoomIn" />
-                <img src="/images/5.png" alt="design5" className="h-[300px] animate__animated animate__zoomIn" />
-                <img src="/images/6.png" alt="design6" className="h-[300px] animate__animated animate__zoomIn" />
+                <a href="https://construction-website-gamma-ruby.vercel.app/">
+                    <div className="flex flex-col justify-start">
+                        {
+                            projects.map((project) =>
+                                <div key={project?.id}>
+                                    <div className="flex flex-col justify-start items-start p-5">
+                                        <div className="flex">{project?.img}</div>
+                                        <h1 className="text-xl font-bold">{project?.name}</h1>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                </a>
             </div>
         </>
 
